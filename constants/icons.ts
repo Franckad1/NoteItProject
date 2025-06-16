1904c34be4f5c70dea9e682da7c63436e4a893fc
Franck <162502379+Franckad1@users.noreply.github.com>
2025-06-16 10:51:26 +0200

    First Commit with the accessible layout

---
+++ constants/icons.ts
@@ -0,0 +1,21 @@
+import arrow from "@/assets/icons/arrow.png";
+import home from "@/assets/icons/home.png";
+import logo from "@/assets/icons/logo.png";
+import logo2 from "@/assets/icons/logo2.png";
+import person from "@/assets/icons/person.png";
+import play from "@/assets/icons/play.png";
+import save from "@/assets/icons/save.png";
+import search from "@/assets/icons/search.png";
+import star from "@/assets/icons/star.png";
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  logo2,
+  save,
+  star,
+  play,
+  arrow,
+};
